5522d6d8510c5efdc386e8cd7452d3f8f727e4f6
Nana Mensah <Nana.mensah1@nhs.net>
2018-10-29 15:37:25 +0000

    Initial dev commit - added readme, dxapp, code.sh

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# dnanexus_GATK_genotypegvcfs
+# Title
+[repo](https://url)
+
+## What does this app do?
+-
+
+## What are typical use cases for this app?
+-
+
+## What inputs are required for this app to run?
+- Input GVCF files
+- Input GVCF file indexes
+- Reference input files for GATK; A fasta file, its index (\*.fai) and its dict file (\*.dict). See [GATK resource bundle](https://software.broadinstitute.org/gatk/download/bundle) for prepared reference files.
+- Intervals list - A file (.bed or .list) containing genomic intervals for processing.
+
+## What does this app output?
+-
+
+## How does this app work?
+-
+
+## References
+
+*Developed by Viapath Genome Informatics*
---
+++ src/code.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# -e = exit on error; -x = output each line that is executed to log; -o pipefail = throw an error if there's an error in pipeline
+set -e -x -o pipefail
+
+function docker_path(){
+	# Replace the home directory in a given string with the docker directory path. 
+	# Allows use of dx_helper variable syntax in mounted docker containers.
+	input_string=$1
+	echo $input_string | sed -e "s,$home_dir,$docker_dir,g"
+}
+
+# Set variables and functions for dx-docker
+home_dir=/home/dnanexus
+docker_dir=/gatk/sandbox
+
+# Download input data
+dx-download-all-inputs
+
+# Move reference genome inputs to the same directory
+mv ${reference_fasta_index_path} ${reference_fasta_dict_path} $(dirname $reference_fasta_path)
+# Move VCF files and their indexes to the home directory
+mv ${input_gvcf_path[@]} ${input_gvcf_index_path} $HOME
+
+# Set helper variables for docker container
+docker_reference_fasta_path=$(docker_path $reference_fasta_path)
+docker_input_gvcf_path=$(docker_path $HOME)
+docker_intervals_list_path=$(docker_path $intervals_list_path)
+docker_output_prefix="RUNFOLDER.combined"
+
+# Set number of cores available
+CORES=$(nproc)
+
+# Pull GATK docker to workstation
+dx-docker pull broadinstitute/gatk:4.0.9.0
+
+# Call GenomicsDBImport
+dx-docker run -v /home/dnanexus/:${docker_dir} broadinstitute/gatk:4.0.9.0 gatk GenomicsDBImport \
+  --genomicsdb-workspace-path gendb://gendb -V ${docker_input_gvcf_path} --L ${docker_intervals_list_path} \
+  --reader-threads ${CORES}
+
+# Call Genotype GVCF
+dx-docker run -v /home/dnanexus/:${docker_dir} broadinstitute/gatk:4.0.9.0 gatk GenotypeGVCFs \
+  -R ${docker_reference_fasta_path} -V gendb://gendb -G StandardAnnotation -O ${docker_dir}/${docker_output_prefix}.vcf
+
+# Create output directories and move respective files
+combined_vcf_out="out/combined_vcf"
+combined_vcf_index_out="out/combined_vcf_index"
+mkdir -p ${combined_vcf_out} && mv ${docker_output_prefix}.vcf ${combined_vcf_out}
+mkdir -p ${combined__vcf_index_out} && mv ${docker_output_prefix}.vcf.idx ${combined_vcf_index_out}
+
+# Upload output data 
+dx-upload-all-outputs
