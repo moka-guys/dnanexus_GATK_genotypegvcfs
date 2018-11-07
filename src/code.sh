f84e8a078cd83f95cc09756e95b32584336a6e46
Nana Mensah <Nana.mensah1@nhs.net>
2018-11-07 11:39:55 +0000

    User gatk docker image asset from DNAnexus

--- src/code.sh
+++ src/code.sh
@@ -13,8 +13,8 @@ dx-docker pull broadinstitute/gatk:4.0.9.0
 mv ${input_gvcfs_path[@]} ${input_gvcfs_index_path[@]} ${reference_fasta_path} \
   ${reference_fasta_index_path} ${reference_fasta_dict_path} ${intervals_list_path} ${HOME}
 
-# Set command for GenomicsDBImport input GVCFs. Example:
-#     "-V sample1.g.vcf -V sample2.g.vcf -V sample3.g.vcf"
+# Set command string for passing multiple input GVCFs to GenomicsDBImport. Example:
+#    $docker_input_gvcfs --> "-V sample1.g.vcf -V sample2.g.vcf -V sample3.g.vcf"
 for gvcf in $(ls *.g.vcf); do
 	docker_input_gvcfs="${docker_input_gvcfs} -V /gatk/sandbox/${gvcf}"
 done
@@ -22,23 +22,26 @@ done
 # Set number of cores available for multi-threading
 CORES=$(nproc)
 
-# Set output VCF prefix based on user app input
+# Set output combined VCF prefix based on user app input
 if [ "${output_vcf_prefix}" = "merge" ]; then
+    # Uses python to merge input vcf file prefixes, delimited by '_' characters, if the merge option is given (default)
     output_vcf_name="$(python -c 'import sys;print("_".join(sys.argv[1:]))' "${input_gvcfs_prefix[@]}")_combined.vcf"
 else
+    # Else uses user output prefix name
     output_vcf_name="${output_vcf_prefix}.vcf"
 fi
 
-# Call GenomicsDBImport
-dx-docker run -v /home/dnanexus:/gatk/sandbox broadinstitute/gatk:4.0.9.0 gatk GenomicsDBImport \
+# Call GenomicsDBImport. This command creates a local database of the variants between input samples
+dx-docker run -v /home/dnanexus:/gatk/sandbox 68b475015074 gatk GenomicsDBImport \
   --genomicsdb-workspace-path /gatk/sandbox/gendb ${docker_input_gvcfs} --L /gatk/sandbox/${intervals_list_name} \
   --reader-threads ${CORES}
 
-# Call GenotypeGVCFs
-dx-docker run -v /home/dnanexus/:/gatk/sandbox broadinstitute/gatk:4.0.9.0 gatk GenotypeGVCFs \
+# Call GenotypeGVCFs. Performs joint-genotyping on all records in a database from GenomicsDBImport,
+# producing a combined multi-sample VCF.
+dx-docker run -v /home/dnanexus/:/gatk/sandbox 68b475015074 gatk GenotypeGVCFs \
   -R /gatk/sandbox/${reference_fasta_name} -V gendb://sandbox/gendb -G StandardAnnotation -O /gatk/sandbox/${output_vcf_name}
 
-# Create output directories and move respective files for upload
+# Create output directories and move output files for upload
 mkdir -p ${HOME}/out/combined_vcf && mv ${output_vcf_name} ${HOME}/out/combined_vcf
 mkdir -p ${HOME}/out/combined_vcf_index && mv ${output_vcf_name}.idx ${HOME}/out/combined_vcf_index
 
